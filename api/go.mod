68442c151f5799086b19167e3947b01915a8d815
Azathoth-X <ranaprikshit25@gmail.com>
2025-01-04 22:54:24 +0530

    add Dockerfile for multi-stage build and update dependencies; enhance URL shortening logic with validation and Redis integration

--- api/go.mod
+++ api/go.mod
@@ -7,10 +7,18 @@ require github.com/gofiber/fiber/v2 v2.52.6
 require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/go-playground/validator/v10 v10.23.0
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/joho/godotenv v1.5.1
--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,9 +1,13 @@
 package routes
 
 import (
+	"os"
 	"time"
 
+	"github.com/Azathoth-X/url-shorten-go-redis/database"
+	"github.com/Azathoth-X/url-shorten-go-redis/helpers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 
 type request struct {
@@ -31,4 +35,54 @@ func ShortenUrl(c *fiber.Ctx) error {
 	//rate-limiting
 
 	//check iif url actual url
+	if !govalidator.IsUrl(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "bruh why no url "})
+	}
+
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"err": "pls no hack"})
+	}
+
+	var id string
+
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+
+	} else {
+		id = body.CustomShort
+
+	}
+
+	r0 := database.CreateClient(0)
+	defer r0.Close()
+
+	val, _ := r0.Get(database.Ctx, id).Result()
+
+	if val != "" {
+		c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"error": "short in use",
+		})
+	}
+
+	if body.Expiry == 0 {
+		body.Expiry = 24
+	}
+
+	err := r0.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"err": "internal server go brr"})
+	}
+
+	res := response{
+		URL:             body.URL,
+		CustomShort:     "",
+		Expiry:          0,
+		XRateRemaining:  10,
+		XRateLimitReset: 30,
+	}
+
+	res.CustomShort = os.Getenv("DOMAIN") + "/" + id
+	return c.Status(fiber.StatusOK).JSON(res)
+
 }
