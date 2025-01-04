84c7d080a667776b14c401fcdf5355e18f190207
Azathoth-X <ranaprikshit25@gmail.com>
2025-01-04 18:45:37 +0530

    made helper funcs

---
+++ api/helpers/helpers.go
@@ -0,0 +1,28 @@
+package helpers
+
+import (
+	"os"
+	"strings"
+)
+
+func EnforceHTTP(url string) string {
+	if url[:4] != "http" {
+		return "http" + url
+	}
+}
+
+func RemoveDomainError(url string) bool {
+	if url != os.Getenv("DOMAIN") {
+		return false
+	}
+	newurl := strings.Replace(url, "http://", "", 1)
+	newurl = strings.Replace(newurl, "https://", "", 1)
+	newurl = strings.Replace(newurl, "www.", "", 1)
+
+	if newurl != os.Getenv("DOMAIN") {
+		return false
+	}
+
+	return true
+
+}
